7d83c7aeddc8b0a9d63122f3e3dc0eab4bf01c7c
Mark A. Ropper <mark@kiltandblade.com>
2018-01-06 18:47:29 +0000

    Starting Cellular Noise

--- source/defineconfig.hpp
+++ source/defineconfig.hpp
@@ -9,11 +9,13 @@
 
 // Having these defined while editing helps keep the Intelli-sense happy, comment
 // them out when building otherwise the compiler will complain about re-definitions
-// #define WN_INCLUDE_FRACTAL_GETSET
-// #define WN_INCLUDE_PERLIN 
-// #define WN_INCLUDE_PERLIN_FRACTAL
-// #define WN_INCLUDE_SIMPLEX
-// #define WN_INCLUDE_SIMPLEX_FRACTAL
+#define WN_INCLUDE_FRACTAL_GETSET
+#define WN_INCLUDE_CELLULAR_GETSET
+#define WN_INCLUDE_PERLIN 
+#define WN_INCLUDE_PERLIN_FRACTAL
+#define WN_INCLUDE_SIMPLEX
+#define WN_INCLUDE_SIMPLEX_FRACTAL
+#define WN_INCLUDE_CELLULAR
 
 // Until JavaScript and WebAssembly support returning doubles (f64) we're stuck with floats
 #ifdef WN_USE_DOUBLES
